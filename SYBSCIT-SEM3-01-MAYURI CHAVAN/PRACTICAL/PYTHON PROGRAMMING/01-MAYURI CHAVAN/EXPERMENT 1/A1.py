19b0fb45c2ef283f0e6f60c47c2cc7167deae986
chavanmayuri01 <110345495+chavanmayuri01@users.noreply.github.com>
2022-07-31 22:14:54 +0530

    Add files via upload

---
+++ SYBSCIT-SEM3-01-MAYURI CHAVAN/PRACTICAL/PYTHON PROGRAMMING/01-MAYURI CHAVAN/EXPERMENT 1/A1.py
@@ -0,0 +1,8 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Hello! enter the name =MAYURI
+enter the age =18
+You will turn 100 in 2104
+>>> 
